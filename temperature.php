0aee3ddf873b10c17cf1be8af8690b697863f4ba
mafe <mfernandarocosta@gmail.com>
2026-02-27 16:28:17 -0300

    Add temperature converter functionality with input handling and result calculation

---
+++ temperature.php
@@ -0,0 +1,36 @@
+<?php
+
+require_once __DIR__ . '/data/temperature.php';
+require_once __DIR__ . '/includes/functions.php';
+
+$value = "";
+$unitFrom = "";
+$unitTo = "";
+$result = null;
+$units = $temperatures;
+$category = 'temperature';
+$pageTitle = "Temperature Converter";
+$currentPage = 'temperature';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Retrieve the data from $POST
+    $value = filter_input(INPUT_POST, 'value', FILTER_SANITIZE_NUMBER_FLOAT);
+    $unitFrom = $_POST['unitFrom'] ?? '';
+    $unitTo =  $_POST['unitTo'] ?? '';
+
+    // Check if they are numbers
+    if ($unitFrom === "kelvin" && $value < 0) {
+        $errorMessage = "Temperature in Kelvin cannot be below zero (Absolute Zero).";
+    } elseif ($value !== false && isset($units[$unitFrom]) && isset($units[$unitTo])) {
+        // Calculates the result 
+        $result = convertUnits((float)$value, $unitFrom, $unitTo, $units, $category);
+    }
+}
+
+include __DIR__ . '/templates/header.php';
+?>
+
+<main>
+    <h2><?php echo $pageTitle; ?></h2>
+    <?php include __DIR__ . '/templates/form.php'; ?>
+</main>
