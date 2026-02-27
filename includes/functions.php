cdebc6d0621f2adc49a8514d1460220182f27b0c
mafe <mfernandarocosta@gmail.com>
2026-02-27 16:29:03 -0300

    Add unit conversion function for temperature with multiple scales

---
+++ includes/functions.php
@@ -0,0 +1,33 @@
+<?php
+
+function convertUnits($value, $from, $to, $unitArray = "temperature" , $category = "linear") {
+    if ($category === 'temperature') {
+        if ($from === 'celsius' && $to === 'fahrenheit') {
+            return ($value * 9/5) + 32;
+        }
+
+        if ($from === 'celsius' && $to === 'kelvin') {
+            return $value + 273.15;
+        }
+
+        if ($from === 'kelvin' && $to === 'fahrenheit') {
+            return ($value - 273.15) * 9/5 + 32;
+        }
+
+        if ($from === 'kelvin' && $to === 'celsius') {
+            return $value - 273.15;
+        }
+
+        if ($from === 'fahrenheit' && $to === 'kelvin') {
+            return ($value - 32) * 5/9 + 273.15;
+        }
+
+        if ($from === 'fahrenheit' && $to === 'celsius') {
+            return ($value - 32) * 5/9;
+        }
+    }
+
+    $baseValue = $value * $unitArray[$from];
+
+    return $baseValue / $unitArray[$to];
+}
