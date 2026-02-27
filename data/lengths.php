5eb678337e763d67b71b9ae0673ccf6e1d10a79f
mafe <mfernandarocosta@gmail.com>
2026-02-27 16:28:43 -0300

    Add length conversion data with metric and imperial units

---
+++ data/lengths.php
@@ -0,0 +1,15 @@
+<?php
+
+$lengths = [
+    "millimeter" => 0.001,
+    "centimeter" => 0.01,
+    "decimeter" => 0.1,
+    "meter" => 1,
+    "decameter" => 10,
+    "hectometer" => 100,
+    "kilometer" => 1000,
+    "inch" => 0.0254,
+    "feet" => 0.3048,
+    "yard" => 0.9144,
+    "mile" => 1609.34
+];
